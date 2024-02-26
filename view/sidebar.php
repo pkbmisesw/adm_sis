17104787518be659583f917aeaa3118bf3db73ac
AltheaIX <althea.ixy@gmail.com>
2024-02-26 11:20:19 +0700

    Admin Panel

---
+++ view/sidebar.php
@@ -0,0 +1,50 @@
+<body>
+<!-- wrapper -->
+<div class="wrapper">
+    <!--sidebar-wrapper-->
+    <div class="sidebar-wrapper" data-simplebar="true">
+        <div class="sidebar-header">
+            <div class="">
+                <img src="../../assets/images/logo-icon.png" class="logo-icon-2" alt="" />
+            </div>
+            <div>
+                <h4 class="logo-text">Syndash</h4>
+            </div>
+            <a href="javascript:;" class="toggle-btn ms-auto"> <i class="bx bx-menu"></i>
+            </a>
+        </div>
+        <!--navigation-->
+        <ul class="metismenu" id="menu">
+            <li>
+                <a href="javascript:;" class="has-arrow">
+                    <div class="parent-icon icon-color-1"><i class="bx bx-home-alt"></i>
+                    </div>
+                    <div class="menu-title">Dashboard</div>
+                </a>
+                <ul>
+                    <li> <a href="index.html"><i class="bx bx-right-arrow-alt"></i>Analytics</a>
+                    </li>
+                    <li> <a href="index2.html"><i class="bx bx-right-arrow-alt"></i>Sales</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-label">Master</li>
+            <li>
+                <a href="../m_users">
+                    <div class="parent-icon icon-color-5"><i class="bx bx-group"></i>
+                    </div>
+                    <div class="menu-title">Master Pengguna</div>
+                </a>
+            </li>
+            <li class="menu-label">Others</li>
+            <li>
+                <a href="../../logout.php" target="_blank">
+                    <div class="parent-icon icon-color-12"><i class="bx bx-log-out"></i>
+                    </div>
+                    <div class="menu-title">Log out</div>
+                </a>
+            </li>
+        </ul>
+        <!--end navigation-->
+    </div>
+    <!--end sidebar-wrapper-->
